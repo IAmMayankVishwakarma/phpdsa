173719a06ebc14d8281c2b16ce30924fa83298a3
Mayank Vishwakarma <aryan8319@gmail.com>
2025-05-11 10:52:46 +0530

    firt trial

--- basic task of string/string/countfrequency.php
+++ basic task of string/string/countfrequency.php
@@ -6,6 +6,25 @@
   <title>countfrequency</title>
 </head>
 <body>
-  
+  <h1>Count Frequency of Characters in a String</h1>
+  <form method="POST">
+    <label for="inputString">Enter a string:</label><br>
+    <input type="text" id="inputString" name="inputString" required><br><br>
+    <input type="submit" value="Count Frequency">
+  </form>
+
+  <?php
+  if ($_SERVER["REQUEST_METHOD"] == "POST") {
+      $inputString = $_POST['inputString'];
+      $frequency = count_chars($inputString, 1);
+          print_r($frequency);
+      echo "<h2>Character Frequency:</h2>";
+      foreach ($frequency as $char => $count) {
+          echo "Character '" . chr($char) . "' appears " . $count . " times.<br>";
+      }
+  }
+  ?>
+
+
 </body>
 </html>
